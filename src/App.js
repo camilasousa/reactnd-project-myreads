2bbc8adfc1cfa6b602e7947734e859f2609ed257
camilasousa <miyuki.sousa@gmail.com>
2018-04-19 22:21:24 -0300

    refactor: organize components in folders
    Separate components in two folders:
    - presenters for the ones that will call BookAPI and keep the response in its state
    - views for the ones that will get props and render it, nothing else.
    
    Also changed App so route componentes receive other components instead of render methods.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Home from './Home';
-import Search from './Search';
+import Home from './presenters/Home';
+import Search from './presenters/Search';
 
 import './App.css';
 
 const App = () => (
   <div className="app">
-    <Route exact path="/" render={Home} />
-    <Route path="/search" render={Search} />
+    <Route exact path="/" component={Home} />
+    <Route path="/search" component={Search} />
   </div>
 );
 
---
+++ src/presenters/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeView from '../views/Home';
+
+class Home extends React.Component {
+  render() {
+    return (<HomeView />);
+  }
+}
+
+export default Home;
